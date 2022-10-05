3ff4903849aad5b9a3b12b3668e63f7c45ef0453
Dizy <dev@dizy.cc>
2022-10-05 01:58:37 -0400

    Change back to original mismatch warning

--- vectors.cu
+++ vectors.cu
@@ -117,8 +117,7 @@ int main(int argc, char *argv[])
 	// Checking the correctness of the GPU part
 	for (i = 0; i < n; i++)
 		if (fabs(temp[i] - c[i]) >= 0.009) // compare up to the second degit in floating point
-			// printf("Element %d in the result array does not match the sequential version\n", i);
-			printf("Element %d in the result array does not match the sequential version: %f != %f\n", i, temp[i], c[i]);
+			printf("Element %d in the result array does not match the sequential version\n", i);[i]);
 
 	// Free the arrays in the host
 	free(a);
